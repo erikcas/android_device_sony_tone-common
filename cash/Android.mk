008a6f1008edd9bec4c7e38a0506cb5b279ffecb
Erik Castricum <git@cas-online.nl>
2020-11-10 10:52:49 +0100

    tone: allow to build cash
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- cash/Android.mk
+++ cash/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_USES_CASH),true)
+ifeq ($(TARGET_USES_CASH_EXTENSION),true)
 
 LOCAL_PATH := $(call my-dir)
 
